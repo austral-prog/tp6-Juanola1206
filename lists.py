1369027f6041568bdb4bf9869f43d5849a123c41
Juanola1206 <jinores@mail.austral.edu.ar>
2025-11-18 19:47:07 -0300

    Add files via upload

--- lists.py
+++ lists.py
@@ -1,20 +1,52 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_remove_elements) == 0:
+        return []
+    new_list = [list_to_remove_elements[i] for i in range(len(list_to_remove_elements))
+                if i not in (0, 4, 5)]
+    return new_list
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    new_list = ["Pink"] + list_to_add_elements + ["Yellow"]
+    return new_list
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    return len(list_to_check) == 0
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    return sorted(list_to_compare1) == sorted(list_to_compare2)
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    new_list = []
+    for idx, sublist in enumerate(list_of_lists_to_modify):
+        n = len(sublist)
+        if n == 0:
+            new_list.append([])
+        elif n == 1:
+            if idx == 1:
+                new_list.append([])
+            else:
+                new_list.append(sublist[:])
+        elif n == 2:
+            new_list.append(sublist[:])
+        elif n == 3:
+            if idx == 0:
+                new_list.append(sublist[:-1])   # quita el último
+            else:
+                new_list.append(sublist[1:])    # quita el primero
+        elif n == 4:
+            if idx == 2:
+                new_list.append(sublist[2:])    # quita los dos primeros
+            else:
+                new_list.append(sublist[1:-1])  # quita primero y último
+        elif n == 5:
+            if idx == 1:
+                new_list.append(sublist[1:-1])  # quita primero y último
+            else:
+                new_list.append(sublist[2:])    # quita los dos primeros
+        else:
+            new_list.append(sublist[2:])        # quita los dos primeros
+    return new_list
